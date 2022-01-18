f95cb29f909605304568de22f49fe2940dcb2885
Koti4 <Koti4@DEV-ASUS>
2022-01-18 23:28:44 +0300

    feat: add validation for config params

--- Task_01-CLI_TOOL/index.js
+++ Task_01-CLI_TOOL/index.js
@@ -10,21 +10,32 @@ const currentConfig = { conf: '', outputFileName: 'outfile.txt', inputFileName: 
 
 const args = process.argv.slice(2);
 
+const validateConfig = (conf) => {
+  const regExp = /(A)|(C[1|0])|(R[1|0])/;
+  for (let i = 0; i < conf.length; i++) {
+    if (conf[i].match(regExp) === null) {
+      process.exit();
+    }
+  }
+};
+
 for (let i = 0; i < args.length; i += 2) {
   const [tempParam, tempVal] = args.slice(i, args.length);
   
   if (configCommand.includes(tempParam)) {
     currentConfig.conf = tempVal.split('-');
   }
-
+  
   if (inputCommand.includes(tempParam)) {
     currentConfig.inputFileName = tempVal;
   }
-
+  
   if (outputCommand.includes(tempParam)) {
     currentConfig.outputFileName = tempVal;
   }
 }
+
+validateConfig(currentConfig.conf);
 
 const tr = currentConfig.conf.map((code) => new CustomTransformStream(code));
 pipeline(process.stdin, ...tr, process.stdout, err => { console.log(err)});
